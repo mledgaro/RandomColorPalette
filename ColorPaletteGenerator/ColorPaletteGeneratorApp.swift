cb1a7ea93ca6e3801541801538543876538ced5c
MAC <mac@MacBook-Pro.local>
2022-08-19 23:03:03 -0500

    first commit

---
+++ ColorPaletteGenerator/ColorPaletteGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPaletteGeneratorApp.swift
+//  ColorPaletteGenerator
+//
+//  Created by MAC on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorPaletteGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
