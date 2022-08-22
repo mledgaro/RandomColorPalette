ba0f4056c13fac98fe9b2f81d1d17fffb3e73b68
MAC <mac@MacBook-Pro.local>
2022-08-22 17:41:03 -0500

    fix api client. add readme and app screenshot

--- ColorPaletteGenerator/Models/ColormindAPI.swift
+++ ColorPaletteGenerator/Models/ColormindAPI.swift
@@ -10,22 +10,43 @@ import Foundation
 struct ColormindAPI
 {
     static private let apiUrl = URL(string: "http://colormind.io/api/")
-    static private let reqParams = "{\"model\": \"default\"}"
+    static private let reqParams: String = "{\"model\": \"default\"}"
     
-    static func fetchRandomPalette() async throws
+    static func fetchRandomPalette() async throws -> String?
     {
         
         var apiRequest = URLRequest(url: apiUrl!)
         
         apiRequest.httpMethod = "POST"
-        apiRequest.httpBody = reqParams.data(using: .utf8)
         apiRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        apiRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+        apiRequest.httpBody = reqParams.data(using: .utf8)
         
-        async let (data, _) = URLSession.shared.data(from: apiUrl!)
+        let (data, _) = try await URLSession.shared.data(for: apiRequest)
+        
+        let response = String(data: data, encoding: .utf8)
+        
+        print(response!)
+        
+        /*{
+            (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                apiResp = dataString
+            }
+            
+        }
+        task.resume()*/
+        
         //let response = try await JSONDecoder().decode(Response.self, from: data)
-        //return response.users
-        let value = try await String(data: data, encoding: .utf8)!
-        print(value)
+        return response
     }
     
 }
